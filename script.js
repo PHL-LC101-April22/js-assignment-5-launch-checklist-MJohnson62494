037d7fc0b46174342f551137d035449ed1c8ce80
Mitchell Johnson <mitchell.johnson624@gmail.com>
2022-07-07 12:01:47 -0400

    Added Code

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+const { addDestinationInfo, formSubmission } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+window.addEventListener("load", function () {
+    let listedPlanets;
+    let listedPlanetsResponse = myFetch();
+
+    listedPlanetsResponse
+    .then(function (result) {
+        listedPlanets = result;
+
+})
+.then(function () {
+    let chosenPlanet = pickPlanet(listedPlanets);
+    addDestinationInfo(
+        document,
+        chosenPlanet.name,
+        chosenPlanet.diameter,
+        chosenPlanet.star,
+        chosenPlanet.distance,
+        chosenPlanet.moons,
+        chosenPlanet.image
+    );
 });
+
+let form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    pilotInput = document.querySelector("input[name=pilotName]");
+    pilotValue = pilotInput.value;
+    coPilotInput = document.querySelector("input[name=coPilotName]");
+    coPilotValue = coPilotInput.value;
+    fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    fuelLevelValue = fuelLevelInput.value;
+    cargoLevelInput = document.querySelector("input[name=cargoLevel]");
+    cargoLevelValue = cargoMassInput.value;
+});
+
+let list = document.getElementById("faultyItems");
+formSubmission(
+    document,
+    list,
+    PilotValue,
+    coPilotValue,
+    fuelLevelValue,
+    cargoMassValue
+);
+});
+
+   
+   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   
